4c7aed2498e33ef798d85688b159761dc55ab67f
Gregg Fine <gregg@greggfinedev.com>
2023-04-20 10:26:39 -0400

    change base urls for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/audiogames/orchestralrange",
+//   base: "/audiogames/orchestralrange",
   // build: {
   //   minify: true,
   // },
