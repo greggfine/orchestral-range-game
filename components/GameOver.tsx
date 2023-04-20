443e179e8bf43155f318dd10c826743f015eb0a4
Gregg Fine <gregg@greggfinedev.com>
2023-04-20 14:14:55 -0400

    add game over lottie animation

--- components/GameOver.tsx
+++ components/GameOver.tsx
@@ -1,26 +1,36 @@
 import styles from "./game-over.module.scss";
 import btnStyles from "./button.module.scss";
-import scoreIcon from "/images/score-black.svg";
-import { motion } from "framer-motion";
+import Lottie from "react-lottie-player";
+import { useEffect, useState } from "react";
+
 type Props = {
   gameState: { score: number; round: number };
   init: () => void;
 };
+
 const GameOver = ({ gameState, init }: Props) => {
+  const [gameOverAnimation, setGameOverAnimation] = useState<any>(null);
+  const [animationLoaded, setAnimationLoaded] = useState(false);
+
+  useEffect(() => {
+    const fetchLottie = async () => {
+      const response = await fetch("lottie/99105-final-gold.json");
+      const lottie = await response.json();
+      setGameOverAnimation(lottie);
+    };
+    fetchLottie();
+  }, []);
+
+  useEffect(() => {
+    if (gameOverAnimation) {
+      setAnimationLoaded(true);
+    }
+  }, [gameOverAnimation]);
+
   return (
     <div className={styles.gameOver}>
-      <motion.h1
-        className={styles.gameOver__heading}
-        initial={{ opacity: 0, scale: 0.8 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.3, delay: 0.3, ease: "easeIn" }}
-      >
-        GAME OVER
-      </motion.h1>
-      <div className={styles.gameOver__scoreWrapper}>
-        <img src={scoreIcon} alt="score" className={styles.gameOver__icon} />
-        <div className={styles.gameOver__score}>{gameState.score}</div>
-      </div>
+      <Lottie loop={false} animationData={gameOverAnimation} play speed={3} />
+      <div className={styles.gameOver__score}>{gameState.score}</div>
       <button onClick={init} className={btnStyles.button}>
         Play Again?
       </button>
