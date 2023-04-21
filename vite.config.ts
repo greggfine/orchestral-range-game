a09bf24206823f9e52d59277830a4eccdc841989
Gregg Fine <gregg@greggfinedev.com>
2023-04-21 13:55:49 -0400

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/orchestral-range-game",
+  // base: "/orchestral-range-game",
   // build: {
   //   minify: true,
   // },
